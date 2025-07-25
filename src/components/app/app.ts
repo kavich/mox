d618045b53b712141fe7bc66b7db6b78abf27fd6
Aleksandr Kalinin <Aleksandr.Kalinin@sony.com>
2025-07-25 15:28:47 +0800

    f

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,39 +1,168 @@
 import $mol_wire_lib from 'mol_wire_lib'
 import { mox_view, type mox_view_content } from "../view/view";
-class Account extends Object {}
-export class Appp extends Object {
-	
-	@$mol_wire_lib.$mol_wire_plex
-	account( id: number ) {
-		return new Account( id )
-	}
-	
+
+class Entity extends Object {
+
+    constructor(
+        readonly id: number
+    ) { super() }
+
+    destructor() { }
+
+    @$mol_wire_lib.$mol_wire_solo
+    data(data = {}) {
+        return data
+    }
+
+    @$mol_wire_lib.$mol_wire_plex
+    value<
+        Field extends keyof ReturnType<this['data']>
+    >(
+        field: Field,
+        value?: ReturnType<this['data']>[Field],
+    ): ReturnType<this['data']>[Field] {
+
+        return this.data(value === undefined
+            ? undefined
+            : {
+                ... this.data(),
+                [field]: value,
+            }
+        )[field as never]
+
+    }
+
 }
 
+
+class Job extends Entity {
+    @$mol_wire_lib.$mol_wire_plex
+    override data(data = { title: '', uid: '' }) {
+        return data
+    }
+
+    uid(uid?: string) {
+        return this.value('uid', uid)
+    }
+    title(title?: string) {
+        return this.value('title', title)
+    }
+
+
+
+}
+
+
 export class Logo extends mox_view {
-   
+
+    @$mol_wire_lib.$mol_wire_solo
     override sub(): readonly mox_view_content[] {
         return [
             "Logo Component"
         ];
     }
-     
+
 }
 
 export class Sidebar extends mox_view {
-    
+
     @$mol_wire_lib.$mol_wire_solo
     Logo() {
         const logo = new Logo();
         return logo
     }
+    @$mol_wire_lib.$mol_wire_solo
     override sub(): readonly mox_view_content[] {
         return [
 
             this.Logo()
         ];
     }
-   
+
+}
+
+export class Column extends mox_view {
+    override dom_name(): string {
+        return "td"
+    }
+
+    @$mol_wire_lib.$mol_wire_solo
+    override sub(): readonly mox_view_content[] {
+        let i = 0;
+        let cols = ['col']
+
+        return cols
+    }
+}
+
+export class Row extends mox_view {
+    override dom_name(): string {
+        return "tr"
+    }
+
+    @$mol_wire_lib.$mol_wire_plex
+    items(id: number): Column {
+        return new Column();
+    }
+
+
+    @$mol_wire_lib.$mol_wire_plex
+    Column(id: number): Column {
+        return new Column();
+    }
+    override sub(): readonly mox_view_content[] {
+
+        let cols = []
+        for (let i = 0; i < 5; i++) {
+
+            const col = this.Column(i);
+            cols.push(col);
+        }
+        return cols
+    }
+}
+export class Table extends mox_view {
+    override dom_name(): string {
+        return "table"
+    }
+    @$mol_wire_lib.$mol_wire_solo
+    items(): Job[] {
+        let items = [];
+        for (let i = 0; i < 10; i++) {
+            items.push(new Job(i));
+        }
+        return items;
+    }
+    @$mol_wire_lib.$mol_wire_plex
+    Row(id: number): Row {
+        return new Row();
+    }
+
+    override sub(): readonly mox_view_content[] {
+        let i = 0;
+        let rows = []
+        for (const item of this.items()) {
+
+            const row = this.Row(i++);
+            rows.push(row);
+        }
+        return rows
+    }
+}
+
+
+export class Main extends mox_view {
+
+    @$mol_wire_lib.$mol_wire_solo
+    Table() {
+        const table = new Table();
+        return table
+    }
+    override sub(): readonly mox_view_content[] {
+        return [
+            this.Table(),
+        ];
+    }
 }
 
 export class App extends mox_view {
@@ -44,10 +173,19 @@ export class App extends mox_view {
 
         return sidebar
     }
+
+    @$mol_wire_lib.$mol_wire_solo
+    Main() {
+        const main = new Main();
+        return main
+    }
+
+
+
     override sub(): readonly mox_view_content[] {
         return [
-            "Hello, World!",
-            this.Sidebar()
+            this.Sidebar(),
+            this.Main(),
         ];
     }
 
--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,6 @@
 //console.log('working')
 import {App} from "@/components/app/app";
-import {Appp} from "@/components/app/app";
+ 
 import { mox_view, type mox_view_content } from "@/components/view/view";
 //const app = new App();
 (window as any)['App'] = App;
@@ -11,6 +11,5 @@ document?.addEventListener(
 		()=> App.autobind(),
 		{ once: true },
 	)
-
-const appp = new Appp();
+ 
 
